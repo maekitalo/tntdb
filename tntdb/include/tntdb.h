83a66d82b4ab96687b1b9ae3fc7a06ffe2edcf70
maekitalo <maekitalo@8aebba32-5d12-0410-a889-bbfed4e15866>
2007-10-02 22:19:16 +0000

    add all including header tntdb.h
    git-svn-id: svn://svn.code.sf.net/p/tntdb/code/trunk@71 8aebba32-5d12-0410-a889-bbfed4e15866

---
+++ tntdb/include/tntdb.h
@@ -0,0 +1,47 @@
+/* 
+ * Copyright (C) 2007 Tommi Maekitalo
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef TNTDB_H
+#define TNTDB_H
+
+#include <tntdb/connect.h>
+#include <tntdb/connection.h>
+#include <tntdb/connectionpool.h>
+#include <tntdb/date.h>
+#include <tntdb/datetime.h>
+#include <tntdb/decimal.h>
+#include <tntdb/error.h>
+#include <tntdb/librarymanager.h>
+#include <tntdb/result.h>
+#include <tntdb/row.h>
+#include <tntdb/statement.h>
+#include <tntdb/time.h>
+#include <tntdb/transaction.h>
+#include <tntdb/value.h>
+
+/**
+ * Main namespace for tntdb.
+ *
+ * All code is in this namespace.
+ */
+
+namespace tntdb
+{
+}
+
+#endif // TNTDB_H
