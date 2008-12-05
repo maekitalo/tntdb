c8dd3a85c2279674e04f56551e905dc6b5dbebad
maekitalo <maekitalo@8aebba32-5d12-0410-a889-bbfed4e15866>
2008-12-05 13:14:51 +0000

    bump so number simplify cxxtools configuration
    git-svn-id: svn://svn.code.sf.net/p/tntdb/code/trunk@150 8aebba32-5d12-0410-a889-bbfed4e15866

--- tntdb/src/librarymanager.cpp
+++ tntdb/src/librarymanager.cpp
@@ -54,7 +54,7 @@ namespace tntdb
       }
       catch (const cxxtools::OpenLibraryFailed& e)
       {
-        log_debug("opening library \"" << d << "\" failed: " << e.what());
+        log_warn("opening library \"" << d << "\" failed: " << e.what());
       }
     }
 
